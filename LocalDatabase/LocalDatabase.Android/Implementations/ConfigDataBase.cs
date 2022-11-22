cc167996b3a7efe04c623a59aecab3ac81ca46d0
Noe <noe@gmail.com>
2022-11-22 17:26:29 -0500

    Agregar archivos de proyecto.

---
+++ LocalDatabase/LocalDatabase.Android/Implementations/ConfigDataBase.cs
@@ -0,0 +1,16 @@
+﻿using LocalDatabase.Droid.Implementations;
+using System.IO;
+using Xamarin.Forms;
+using LocalDatabase.Interfaces;
+
+[assembly: Dependency(typeof(ConfigDataBase))]
+namespace LocalDatabase.Droid.Implementations
+{
+    public class ConfigDataBase : IConfigDataBase 
+    {
+        public string GetFullPath(string databaseFileName)
+        {
+            return Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), databaseFileName);
+        }
+    }
+}
---
+++ LocalDatabase/LocalDatabase.iOS/implementations/ConfigDataBase.cs
@@ -0,0 +1,17 @@
+﻿using LocalDatabase.iOS.Implementations;
+using LocalDatabase.Interfaces;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ConfigDataBase))]
+namespace LocalDatabase.iOS.Implementations
+{
+    public class ConfigDataBase : IConfigDataBase
+    {
+        public string GetFullPath(string databaseFileName)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseFileName);
+        }
+    }
+}
